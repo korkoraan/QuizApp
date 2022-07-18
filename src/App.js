4788b9e3ec6cedd9f33a5c28559bb4f84ac6168c
Korkoraan <hemul888@gmail.com>
2022-07-18 20:48:29 +0600

    add navigation between screens via React Navigation

--- src/App.js
+++ src/App.js
@@ -1,95 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import type {Node} from 'react';
-import {
-  KeyboardAvoidingView,
-  Platform,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AuthScreen from "./AuthScreen";
+import HomeScreen from "./HomeScreen";
 
-const SubmitBtn = () => {
-  return (
-    <TouchableOpacity style={styles.submit_btn}>
-      <Text style={styles.pseudo_icon}>-></Text>
-    </TouchableOpacity>
-  );
-};
+const Stack = createNativeStackNavigator();
+
 const App: () => Node = () => {
-  const [nickname, setNickname] = useState('');
-
-  const checkNickname = () => {
-    return nickname !== '' ? SubmitBtn() : null;
-  };
-  const headerText = () => {
-    return nickname === '' ? 'Welcome!' : 'Welcome, ' + nickname + '!';
-  };
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      style={styles.screen}>
-      <View style={styles.auth_card}>
-        <Text style={styles.text}> {headerText()} </Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder={' name'}
-          onChangeText={text => {
-            setNickname(text);
-          }}
-        />
-      </View>
-      <View>{checkNickname()}</View>
-    </KeyboardAvoidingView>
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName={"Auth"} screenOptions={{headerShown: false}}>
+            <Stack.Screen name="Auth" component={AuthScreen} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  auth_card: {
-    flex: 0.3,
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    width: 250,
-    marginTop: 100,
-  },
-  text: {
-    color: 'black',
-  },
-  textInput: {
-    flex: 0.2,
-    height: 20,
-    width: 230,
-    borderRadius: 2,
-    borderColor: 'black',
-    borderWidth: 0.5,
-    marginTop: 10,
-  },
-  submit_btn: {
-    position: 'absolute',
-    marginTop: 40,
-    marginStart: 70,
-    borderRadius: 15,
-    width: 50,
-    height: 50,
-    borderColor: 'black',
-    borderWidth: 1,
-  },
-  pseudo_icon: {
-    alignSelf: 'center',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-});
 
 export default App;
