87edaa56cb6c3d72577442c75e969b0e6e3add0a
Korkoraan <hemul888@gmail.com>
2022-07-27 19:08:02 +0600

    add firebase support

--- src/AuthScreen.js
+++ src/AuthScreen.js
@@ -8,6 +8,43 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
+import firestore from '@react-native-firebase/firestore';
+
+const userExists = async username => {
+  const user = await firestore().collection('users').doc(username).get();
+  return user;
+};
+
+const getUsers = async () => {
+  firestore()
+    .collection('users')
+    .doc('413gnOIQ3X3NImBjMHNR')
+    .get()
+    .then(data => console.log(data._data.name))
+    .catch(err => console.error(err));
+};
+
+const validUser = user_info => {
+  if (!userExists(user_info.name)) {
+    return false;
+  }
+  const usernameRegex = /^[a-zA-Z0-9]+$/;
+  return usernameRegex.test(user_info.name);
+};
+
+const addUser = user_info => {
+  if (!validUser(user_info)) {
+    return false;
+  }
+  firestore()
+    .collection('users')
+    .add({
+      name: user_info.name,
+    })
+    .then(() => {
+      console.log('User added!');
+    });
+};
 
 export default function AuthScreen({navigation}) {
   const SubmitBtn = () => {
@@ -15,9 +52,18 @@ export default function AuthScreen({navigation}) {
       <TouchableOpacity
         style={styles.submit_btn}
         onPress={() => {
+          // addUser(nickname);
           navigation.navigate('Home');
         }}>
         <Text style={styles.pseudo_icon}>-></Text>
+      </TouchableOpacity>
+    );
+  };
+
+  const TestBtn = () => {
+    return (
+      <TouchableOpacity style={styles.test_btn} onPress={getUsers}>
+        <Text style={styles.pseudo_icon}>?</Text>
       </TouchableOpacity>
     );
   };
@@ -44,6 +90,7 @@ export default function AuthScreen({navigation}) {
           }}
         />
       </View>
+      <TestBtn />
       <View>{checkNickname()}</View>
     </KeyboardAvoidingView>
   );
@@ -81,6 +128,15 @@ const styles = StyleSheet.create({
   },
   submit_btn: {
     position: 'absolute',
+    marginTop: 40,
+    marginStart: 70,
+    borderRadius: 15,
+    width: 50,
+    height: 50,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  test_btn: {
     marginTop: 40,
     marginStart: 70,
     borderRadius: 15,
